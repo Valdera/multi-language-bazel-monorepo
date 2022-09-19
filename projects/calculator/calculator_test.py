ca7622ea87bcfd6cb0918113f4d090dcbffe41e4
Valdera <f.valdera@yahoo.co.id>
2022-09-20 02:03:59 +0700

    python

--- projects/calculator/calculator_test.py
+++ projects/calculator/calculator_test.py
@@ -5,7 +5,7 @@ from projects.calculator.calculator import Calculator
 class TestSum(unittest.TestCase):
     def test_sum(self):
         calculator = Calculator()
-        self.assertEqual(calculator.add(1, 2))
+        self.assertEqual(calculator.add(1, 2), 3)
 
 
 if __name__ == "__main__":
