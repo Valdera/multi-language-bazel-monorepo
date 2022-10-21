d682b7924790e964e5daca752063ebe60e7b6c7e
Valdera <f.valdera@yahoo.co.id>
2022-10-22 02:25:21 +0700

    Add go example

---
+++ projects/go_web/main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/Valdera/multi-language-bazel-monorepo/projects/go_hello_world"
+	"github.com/gorilla/mux"
+)
+
+func YourHandler(w http.ResponseWriter, r *http.Request) {
+	log.Println("Received request")
+	w.Write([]byte(go_hello_world.HelloWorld() + " Bazoku is cool!!"))
+}
+
+func getPort() string {
+	port := os.Getenv("PORT")
+	if port == "" {
+		return "8080"
+	}
+
+	return port
+}
+
+func main() {
+	r := mux.NewRouter()
+	// Routes consist of a path and a handler function.
+	r.HandleFunc("/", YourHandler)
+	// Bind to a port and pass our router in
+	port := getPort()
+	log.Println("Going to listen on port: " + port)
+	log.Fatal(http.ListenAndServe(":"+port, r))
+}
